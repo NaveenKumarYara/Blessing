50477008a5347f9f27965105437a3ca59efb85e4
NaveenKumarYara <93984381+NaveenKumarYara@users.noreply.github.com>
2021-11-09 17:54:18 +0530

    BlessingsBasic

---
+++ js/general.js
@@ -0,0 +1,214 @@
+/* Custom General jQuery
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+;(function($, window, document, undefined) {
+	//Genaral Global variables
+	//"use strict";
+	var $win = $(window);
+	var $doc = $(document);
+	var $winW = function(){ return $(window).width(); };
+	var $winH = function(){ return $(window).height(); };
+	var $screensize = function(element){  
+			$(element).width($winW()).height($winH());
+		};
+		
+		var screencheck = function(mediasize){
+			if (typeof window.matchMedia !== "undefined"){
+				var screensize = window.matchMedia("(max-width:"+ mediasize+"px)");
+				if( screensize.matches ) {
+					return true;
+				}else {
+					return false;
+				}
+			} else { // for IE9 and lower browser
+				if( $winW() <=  mediasize ) {
+					return true;
+				}else {
+					return false;
+				}
+			}
+		};
+
+	$doc.ready(function() {
+/*--------------------------------------------------------------------------------------------------------------------------------------*/		
+		// Remove No-js Class
+		$("html").removeClass('no-js').addClass('js');
+		
+		
+		
+		/* Get Screen size
+		---------------------------------------------------------------------*/
+		$win.on('resize', function(){
+			$screensize('your selector');	
+		}).resize();	
+	
+		
+		/* Menu ICon Append prepend for responsive
+		---------------------------------------------------------------------*/
+		$(window).on('resize', function(){
+			if (screencheck(767)) {
+				if(!$('#menu').length){
+					$('#mainmenu').prepend('<a href="#" id="menu" class="menulines-button"><span class="menulines"></span> <em>Menu</em></a>');
+				}
+			} else {
+				$("#menu").remove();
+			}
+		}).resize();
+
+		/* This adds placeholder support to browsers that wouldn't otherwise support it. 
+		---------------------------------------------------------------------*/
+		if (document.createElement("input").placeholder === undefined) {
+			var active = document.activeElement;
+			$(':text').focus(function () {
+				if ($(this).attr('placeholder') !== '' && $(this).val() === $(this).attr('placeholder')) {
+					$(this).val('').removeClass('hasPlaceholder');
+				}
+			}).blur(function () {
+				if ($(this).attr('placeholder') !== '' && ($(this).val() === '' || $(this).val() === $(this).attr('placeholder'))) {
+					$(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+				}
+			});
+			$(':text').blur();
+			$(active).focus();
+			$('form:eq(0)').submit(function () {
+				$(':text.hasPlaceholder').val('');
+			});
+		}
+		
+		
+		/* Tab Content box 
+		---------------------------------------------------------------------*/
+		var tabBlockElement = $('.tab-data');
+			$(tabBlockElement).each(function() {
+				var $this = $(this),
+					tabTrigger = $this.find(".tabnav li"),
+					tabContent = $this.find(".tabcontent");
+					var textval = [];
+					tabTrigger.each(function() {
+						textval.push( $(this).text() );
+					});	
+				$this.find(tabTrigger).first().addClass("active");
+				$this.find(tabContent).first().show();
+
+				
+				$(tabTrigger).on('click',function () {
+					$(tabTrigger).removeClass("active");
+					$(this).addClass("active");
+					$(tabContent).hide().removeClass('visible');
+					var activeTab = $(this).find("a").attr("data-rel");
+					$this.find('#' + activeTab).fadeIn('normal').addClass('visible');
+								
+					return false;
+				});
+			
+				var responsivetabActive =  function(){
+				if (screencheck(767)){
+					if( !$('.tabMobiletrigger').length ){
+						$(tabContent).each(function(index) {
+							$(this).before("<h2 class='tabMobiletrigger'>"+textval[index]+"</h2>");	
+							$this.find('.tabMobiletrigger:first').addClass("rotate");
+						});
+						$('.tabMobiletrigger').click('click', function(){
+							var tabAcoordianData = $(this).next('.tabcontent');
+							if($(tabAcoordianData).is(':visible') ){
+								$(this).removeClass('rotate');
+								$(tabAcoordianData).slideUp('normal');
+								//return false;
+							} else {
+								$this.find('.tabMobiletrigger').removeClass('rotate');
+								$(tabContent).slideUp('normal');
+								$(this).addClass('rotate');
+								$(tabAcoordianData).not(':animated').slideToggle('normal');
+							}
+							return false;
+						});
+					}
+						
+				} else {
+					$('.tabMobiletrigger').remove();
+					$this.find(tabTrigger).removeClass("active").first().addClass('active');
+					$this.find(tabContent).hide().first().show();			
+				}
+			};
+			$(window).on('resize', function(){
+				if(!$this.hasClass('only-tab')){
+					responsivetabActive();
+				}
+			}).resize();
+		});
+		
+		/* Accordion box JS
+		---------------------------------------------------------------------*/
+		$('.accordion-databox').each(function() {
+			var $accordion = $(this),
+				$accordionTrigger = $accordion.find('.accordion-trigger'),
+				$accordionDatabox = $accordion.find('.accordion-data');
+				
+				$accordionTrigger.first().addClass('open');
+				$accordionDatabox.first().show();
+				
+				$accordionTrigger.on('click',function (e) {
+					var $this = $(this);
+					var $accordionData = $this.next('.accordion-data');
+					if( $accordionData.is($accordionDatabox) &&  $accordionData.is(':visible') ){
+						$this.removeClass('open');
+						$accordionData.slideUp(400);
+						e.preventDefault();
+					} else {
+						$accordionTrigger.removeClass('open');
+						$this.addClass('open');
+						$accordionDatabox.slideUp(400);
+						$accordionData.slideDown(400);
+					}
+				});
+		});
+			
+		/*Mobile menu click
+		---------------------------------------------------------------------*/
+		$(document).on('click',"#menu", function(){
+			$(this).toggleClass('menuopen');
+			$(this).next('ul').slideToggle('normal');
+			return false;
+		});
+		
+
+		/*Slider
+		---------------------------------------------------------------------*/
+		$('.slider').slick({
+			infinite: true,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			dots: false,
+			arrows: true,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 1,
+						infinite: true,
+						dots: true
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+				// You can unslick at a given breakpoint now by adding:
+				// settings: "unslick"
+				// instead of a settings object
+			]
+		});
+		
+/*--------------------------------------------------------------------------------------------------------------------------------------*/		
+	});	
+
+/*All function nned to define here for use strict mode
+----------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+	
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+})(jQuery, window, document);
